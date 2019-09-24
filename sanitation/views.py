994c6cd282d3baa83f16961c1790591c9f66f156
nimosaid <saidnimo123@gmail.com>
2019-09-24 17:20:15 +0300

    writting views.py code

--- sanitation/views.py
+++ sanitation/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
+from .models import *
+from .forms import *
+from django.shortcuts import get_object_or_404
+
 
 # Create your views here.
+#first page - signup page
+def signup(request):
+    if request.method=='POST':
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+        return redirect('login')
+
+#landing page - home page
+def index(request):
+
+    index_path = Project.objects.all()
+    return render(request,'index.html',locals())
+
+
+
+
+# def search_project(request):
+
+#     if 'project' in request.GET and request.GET["project"]:
+#         search_term = request.GET.get("project")
+#         searched_project = Project.search_by_name(search_term)
+#         message = f"{search_term}"
+
+#         return render(request, 'search.html',locals())
+
+#     else:
+#         message = "You haven't searched for any term"
+#         return render(request, 'search.html',locals())
+

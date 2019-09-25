6bcf9c4973ec8b973e7b2be1e609da11a59b4ece
nimosaid <saidnimo123@gmail.com>
2019-09-25 16:17:35 +0300

    working on home page UI

--- sanitation/views.py
+++ sanitation/views.py
@@ -1,26 +1,17 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from .models import *
-from .forms import *
 from django.shortcuts import get_object_or_404
 
 
-# Create your views here.
 
-def login(request):
-    if request.method=='POST':
-        form = UserCreationForm(request.POST)
-
-        if form.is_valid():
-            form.save()
-        return render('login')
 
 #landing page - home page
 def index(request):
 
-    index_path = Project.objects.all()
-    return render(request,'index.html',locals())
-
+    return render(request,'index.html')
+# def welcome(request):
+#     return HttpResponse('Welcome to the Moringa Tribune')
 
 
 

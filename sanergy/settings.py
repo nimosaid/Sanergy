cdaa04833b4f4fd59aae54fa084f3534d2c29c63
vincentouma <vinceoumah@gmail.com>
2019-09-26 09:29:42 +0300

    add sanitation to installeled app

--- sanergy/settings.py
+++ sanergy/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'sanitation'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -107,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 

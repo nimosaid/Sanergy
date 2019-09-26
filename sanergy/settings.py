79756d7da86f3a94869f502ee8c0fc4a2342e21e
vincentouma <vinceoumah@gmail.com>
2019-09-26 09:26:48 +0300

    configure database to settings

--- sanergy/settings.py
+++ sanergy/settings.py
@@ -75,8 +75,10 @@ WSGI_APPLICATION = 'sanergy.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'sanergy',
+        'USER': 'moringa',
+    'PASSWORD':'vinceobindi1005',
     }
 }
 

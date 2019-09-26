77be5d34c20731b869e9ddd018dfe6d4355073fe
nimosaid <saidnimo123@gmail.com>
2019-09-26 08:19:29 +0300

    dealing with landing page ui

--- sanergy/settings.py
+++ sanergy/settings.py
@@ -119,4 +119,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+MEDIA_URL='/media/'
+MEDIA_ROOT=os.path.join(BASE_DIR,'media')
+STATICFILES_DIRS = (
+	os.path.join(BASE_DIR, 'static'),
+)

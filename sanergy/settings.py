02352237d1ad8d6aca689e358cd68ddc4535d3a5
vincentouma <vinceoumah@gmail.com>
2019-09-26 14:49:13 +0300

    switched to bootstrap3

--- sanergy/settings.py
+++ sanergy/settings.py
@@ -31,8 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'sanitation'
-    'bootstrap4'
+    'sanitation',
+    'bootstrap3',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

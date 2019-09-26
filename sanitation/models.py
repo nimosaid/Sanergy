1a0a0250404e8d14bd891b03a63ad28cde22d8ea
magechaP <magechapeter@gmail.com>
2019-09-26 18:41:13 +0300

    create user models

--- sanitation/models.py
+++ sanitation/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+
 # Create your models here.
+class Customer(models.Model):
+    account_number= models.IntegerField(default=0)
+    phone_number=models.IntegerFiled(max_length=10,default=0000000000)
+    name=models.CharField(default=0)

94974421a7d571564ef540762412e20a93d988a3
magechaP <magechapeter@gmail.com>
2019-09-26 17:19:12 +0300

    update customer model to include customer phone no.

--- user/models.py
+++ user/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 # Create your models here.
-class User1(models.Model):
+class Customer(models.Model):
     account_number= models.IntegerField(default=0)
     phone_number=models.IntegerFiled(max_length=10,default=0000000000)
     name=models.CharField(default=0)

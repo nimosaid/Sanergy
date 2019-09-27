2f3a9f0dd26c91e617f868bbbafb70d266654133
nimosaid <saidnimo123@gmail.com>
2019-09-27 10:40:22 +0300

    home page done

--- sanitation/urls.py
+++ sanitation/urls.py
@@ -5,5 +5,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$',views.index, name="homePage"),
+    url(r'^$',views.index, name="homePage")
    
 ]
--- sanitation/views.py
+++ sanitation/views.py
@@ -10,21 +10,4 @@ from django.shortcuts import get_object_or_404
 def index(request):
 
     return render(request,'index.html')
-# def welcome(request):
-#     return HttpResponse('Welcome to the Moringa Tribune')
-
-
-
-# def search_project(request):
-
-#     if 'project' in request.GET and request.GET["project"]:
-#         search_term = request.GET.get("project")
-#         searched_project = Project.search_by_name(search_term)
-#         message = f"{search_term}"
-
-#         return render(request, 'search.html',locals())
-
-#     else:
-#         message = "You haven't searched for any term"
-#         return render(request, 'search.html',locals())
 

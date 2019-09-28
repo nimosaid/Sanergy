6802aae3ada83d2bc48a7b620a3ba2cad2a7d4c3
vincentouma <vinceoumah@gmail.com>
2019-09-28 22:00:36 +0300

    Add toilet url

--- sanitation/urls.py
+++ sanitation/urls.py
@@ -13,6 +13,7 @@ from . import views
 urlpatterns = [
     url(r'^$',views.index, name="homePage"),
     url(r'^payment', views.payment, name='payment'),
+    url(r'^toilet', views.toilet, name='toilet'),
     url('access/token', views.getAccessToken, name='get_mpesa_access_token'),
     url('online/lipa', views.lipa_na_mpesa_online, name='lipa_na_mpesa'),
     

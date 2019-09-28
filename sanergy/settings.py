e4821d06a7712185e101c62471b612c511aa5d4a
nimosaid <saidnimo123@gmail.com>
2019-09-28 23:52:13 +0300

    message

--- sanergy/settings.py
+++ sanergy/settings.py
@@ -79,8 +79,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'sanergy',
-        'USER': 'postgres',
-    'PASSWORD':'levertco',
+        'USER': 'nimo',
+    'PASSWORD':'brayo',
     }
 }
 

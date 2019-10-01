ca8063d564cd3f7c489e92260dae5831253d29fa
vincentouma <vinceoumah@gmail.com>
2019-10-01 12:28:33 +0300

    add master

--- sanergy/settings.py
+++ sanergy/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-from celery import Celery
+# from celery import Celery
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)

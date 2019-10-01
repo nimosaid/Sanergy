1a4475d95688729c236640548bbc01ce94f7b5f6
vincentouma <vinceoumah@gmail.com>
2019-10-01 09:42:59 +0300

    Create pymentapiview url

--- sanitation/urls.py
+++ sanitation/urls.py
@@ -16,6 +16,7 @@ urlpatterns = [
     url(r'^toilet', views.toilet, name='toilet'),
     url('access/token', views.getAccessToken, name='get_mpesa_access_token'),
     url('online/lipa', views.lipa_na_mpesa_online, name='lipa_na_mpesa'),
-    
+    url(r'^api/payment/$', views.PaymentList.as_view())
+
 
 ]

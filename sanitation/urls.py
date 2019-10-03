8596ad39fca00fb4efb7e004c1d25f163874672e
nimosaid <saidnimo123@gmail.com>
2019-10-03 21:29:58 +0300

    adding html pages

--- sanitation/urls.py
+++ sanitation/urls.py
@@ -24,6 +24,11 @@ urlpatterns = [
     url(r'^confirmation/$', views.confirmation, name='confrimation'),
 
     url(r'^bills',views.bills,name='bills')
+#profile
+    url(r'^my-profile/',views.my_profile, name='my-profile'),
+    url(r'^user/(?P<username>\w{0,50})',views.user_profile,name='user-profile'),
+    url(r'^create/profile$',views.create_profile, name='create-profile'),
+    url(r'^update/profile$',views.update_profile, name='update-profile'),
 
 ]
 if settings.DEBUG:
--- sanitation/views.py
+++ sanitation/views.py
@@ -40,6 +40,64 @@ def index(request):
     return render(request,'index.html',locals())
 
 
+@login_required(login_url='/accounts/login/')
+def my_profile(request):
+    current_user=request.user
+    profile =Profile.objects.get(username=current_user)
+    return render(request,'profile/user_profile.html',{"profile":profile})
+
+
+@login_required(login_url='/accounts/login/')
+def user_profile(request,username):
+    user = User.objects.get(username=username)
+    profile =Profile.objects.get(username=user)
+
+@login_required(login_url='/accounts/login/')
+def create_profile(request):
+    current_user=request.user
+    if request.method=="POST":
+        form =ProfileForm(request.POST,request.FILES)
+        if form.is_valid():
+            profile = form.save(commit = False)
+            profile.username = current_user
+            profile.save()
+        return HttpResponseRedirect('/')
+    else:
+        form = ProfileForm()
+        return render(request,'profile/profile_form.html',{"form":form})
+    
+
+@login_required(login_url='/accounts/login/')
+def update_profile(request):
+    current_user=request.user
+    if request.method=="POST":
+        instance = Profile.objects.get(username=current_user)
+        form =ProfileForm(request.POST,request.FILES,instance=instance)
+        if form.is_valid():
+            profile = form.save(commit = False)
+            profile.username = current_user
+            profile.save()
+
+        return redirect('index')
+
+    elif Profile.objects.get(username=current_user):
+        profile = Profile.objects.get(username=current_user)
+        form = ProfileForm(instance=profile)
+    else:
+        form = ProfileForm()
+
+    return render(request,'profile/update_profile.html',{"form":form})
+
+# def login(request):
+#     if request.method=='POST':
+#         form = UserCreationForm(request.POST)
+
+#         if form.is_valid():
+#             form.save()
+#         return render('login')
+
+
+
 def payment(request):
     if request.method == 'POST':
         form = PaymentForm(request.POST,request.FILES)

8daf6946f4602fe3537cc44827ad02c0e5842458
nimosaid <saidnimo123@gmail.com>
2019-10-03 21:25:52 +0300

    adding html pages

--- celery.sh
+++ celery.sh
@@ -1,7 +1,5 @@
-<<<<<<< HEAD
+
 
   
 celery -A config worker -c 5 --loglevel=info -Q b2c_result,c2b_confirmation,c2b_validation,online_checkout_request,online_checkout_callback
-=======
-celery -A config worker -c 5 --loglevel=info -Q b2c_result,c2b_confirmation,c2b_validation,online_checkout_request,online_checkout_callback
->>>>>>> 555a810b9e5b10dfa11c1477238b0adeaf84a3f7
+
